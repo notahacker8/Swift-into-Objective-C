9f30a5b4cdf0fcd904886a459c5f9b94833a5668
King Doggo <77363964+notahacker8@users.noreply.github.com>
2021-02-08 02:52:47 -0700

    Create main.m

---
+++ Proj/main.m
@@ -0,0 +1,13 @@
+
+
+//There's a hidden swift header file when you have swift in your program.
+//It's named: [Project Name]-Swift.h
+
+#import "Proj-Swift.h"
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    [SwiftModule ExecuteSwift];
+    return 0;
+}
